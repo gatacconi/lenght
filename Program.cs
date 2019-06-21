c85cf86efdecf9e530f714576488fade31350cfc
Gabriel Bernardes Tacconi Lourenço <gabrieltacconi147@gmail.com>
2019-06-21 15:51:46 -0300

    lenght

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lenght
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int quantidade;
+
+            Console.WriteLine("DIGITE O NOME ");
+            nome = Console.ReadLine();
+
+            quantidade = nome.Length;
+
+
+            Console.WriteLine("\n O nome: " + nome + " tem " + quantidade + " letras");
+        }
+    }
+}
